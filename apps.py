cf444ce4de8e6bfb462923fd355d9c949888cc16
Sholpina <30983083+Sholpina@users.noreply.github.com>
2022-10-15 21:54:28 +0400

    Update

--- apps.py
+++ apps.py
@@ -1,8 +1,6 @@
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
-
 
 
 SECRET_KEY = "some secret key"
@@ -15,36 +13,42 @@ db = SQLAlchemy(app)
 
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return '<h1>Hello everybody!</h1>'
-    
+    return render_template("home.html")
+
+
 @app.route('/loginuser')
 def loginuser():
-    return '<h1>loginuser!</h1>'
+    return render_template("loginuser.html")
+
 
 @app.route('/signupuser')
 def signupuser():
-    return '<h1>signupuser!</h1>'
+    return render_template("signupuser.html")
+
 
 @app.route('/currenttodos')
 def currenttodos():
-    return '<h1>currenttodos!</h1>'
+    return render_template("currenttodos.html")
+
 
 @app.route('/createtodo')
 def createtodos():
-    return '<h1>createtodo!</h1>'
+    return render_template("createtodos.html")
+
 
 @app.route('/completedtodos')
 def completedtodos():
-    return '<h1>completedtodos!</h1>'
+    return render_template("completedtodos.html")
+
 
 @app.route('/viewtodo')
 def viewtodo():
-    return '<h1>vviewtodo!</h1>'
+    return render_template("viewtodo.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
 
 db.create_all()
-
