c0639341391ceb6ef046403bacf21d8def7db356
Sholpina <30983083+Sholpina@users.noreply.github.com>
2022-10-15 20:58:26 +0400

    Added new rout

--- apps.py
+++ apps.py
@@ -1,4 +1,4 @@
-from unicodedata import name
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,13 +13,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 @app.route('/')
 def home():
-        return "<h1>Hello everybody!</h1>"
+    return '<h1>Hello everybody!</h1>'
+    
+@app.route('/loginuser')
+def loginuser():
+    return '<h1>loginuser!</h1>'
+
+@app.route('/signupuser')
+def signupuser():
+    return '<h1>signupuser!</h1>'
+
+@app.route('/currenttodos')
+def currenttodos():
+    return '<h1>currenttodos!</h1>'
+
+@app.route('/createtodo')
+def createtodos():
+    return '<h1>createtodo!</h1>'
+
+@app.route('/completedtodos')
+def completedtodos():
+    return '<h1>completedtodos!</h1>'
+
+@app.route('/viewtodo')
+def viewtodo():
+    return '<h1>vviewtodo!</h1>'
 
 
 if __name__ == "__main__":
-        app.run(debug=True)
+    app.run(debug=True)
 
 db.create_all()
 
